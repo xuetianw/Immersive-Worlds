51bd30653dcb3f5a349ccb685f26d41fb5384585
Jan Ycasas <ycasas.jan@gmail.com>
2019-01-18 19:44:08 -0800

    added a vector to whom the message will be received by

--- lib/message/src/Message.cpp
+++ lib/message/src/Message.cpp
@@ -1,10 +1,11 @@
 #include <Message.h>
 #include <string>
 
-Message::Message(int author_user_id, std::string type, std::string message_text) {
+Message::Message(int author_user_id, std::string type, std::string message_text, std::vector<int> to_user_id) {
     this->author_user_id    = author_user_id;
     this->message_text      = message_text;
     this->type              = getMessageType(type);
+    this->to_user_id        = to_user_id;
 }
 
 MessageType Message::getMessageType(std::string type) {
@@ -22,4 +23,8 @@ MessageType Message::getMessageType(std::string type) {
 
     return result;
 }  
+
+void Message::addUser(int id) {
+    this->to_user_id.push_back(id);
+}
 
