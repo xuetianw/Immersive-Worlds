eb17c727d7f5e4d605f504076d9ef57946cbe92d
mcw19 <mcw19@sfu.ca>
2019-01-18 20:27:16 -0800

    removed nlohmann from the networking library and added todo to remove test function

--- tools/chatclient/chatclient.cpp
+++ tools/chatclient/chatclient.cpp
@@ -38,7 +38,7 @@ void jsonTest() {
 int
 main(int argc, char* argv[]) {
   if (argc < 3) {
-      jsonTest();
+      jsonTest(); // TODO remove testing function
     std::cerr << "Usage: \n  " << argv[0] << " <ip address> <port>\n"
               << "  e.g. " << argv[0] << " localhost 4002\n";
     return 1;
