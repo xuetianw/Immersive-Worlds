d5fc23eb25b63c7cc8681452f87ae5c15a02f87e
Jan Ycasas <ycasas.jan@gmail.com>
2019-01-24 19:16:43 -0800

    Created Area

--- lib/channel/include/World.h
+++ lib/channel/include/World.h
@@ -2,17 +2,17 @@
 // Created by Jan Ycasas on 2019-01-24.
 //
 
-#ifndef CHANNEL_AREA_H
-#define CHANNEL_AREA_H
+#ifndef CHANNEL_WORLD_H
+#define CHANNEL_WORLD_H
 
 #include <unordered_map>
-#include <Room.h>
+#include <Area.h>
 
 namespace channel {
-    class Area {
+    class World {
         public:
-            Area();
-            ~Area();
+            World();
+            ~World();
 
             /**
              * Gets the Id of the area
@@ -24,16 +24,16 @@ namespace channel {
             /**
              * Adds a new area to the unordered_map.
              * 
-             * @param Room room: room object that will be added to the map
+             * @param Area area: room object that will be added to the map
              * 
              * @returns true = inserted in the map. false = id already exists in map
              */
-            bool addRoom(Room room);
+            bool addArea(Area area);
 
         private:
-            const int _id = 0; //TODO make area id unique
-            std::unordered_map<int, Room> _rooms;
+            const int _id = 0; //TODO make world id unique
+            std::unordered_map<int, Area> _areas;
     };
 } // namespace channel
 
-#endif //CHANNEL_AREA_H
+#endif //CHANNEL_WORLD_H
---
+++ lib/channel/src/World.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Jan Ycasas on 2019-01-24.
+//
+
+#include "World.h"
+
+channel::World::World() {
+    // TODO 
+}
+
+channel::World::~World() {
+    // TODO
+}
+
+bool channel::World::addArea(Area area) {
+    auto areaId     = area.getId();
+    auto newArea    = std::move(area);
+    auto didInsert  = _areas.insert({areaId, newArea}).second;
+
+    return didInsert;
+}
+
+int channel::World::getId() const {
+    // TODO make room ID unique
+    return this->_id;
+}
+
+
