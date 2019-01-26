40a4e73c78126c835b420261042dcde5cf67739e
rzshen <rzshen@sfu.ca>
2019-01-25 17:05:59 -0800

    remove warning

--- lib/inventory/src/Inventory.cpp
+++ lib/inventory/src/Inventory.cpp
@@ -8,18 +8,19 @@ Inventory::Inventory(std::vector<InventoryItem> setOfItems): _inventoryItems(set
 
 std::vector<InventoryItem> Inventory::getItems() const { return _inventoryItems; }
 
+//TODO: look into differentiating derived class and finish this
 void Inventory::addItems(std::vector<InventoryItem> itemsToAdd){
-    //add items until vector is empty
-    while( !itemsToAdd.empty()){
-
-        //retrieve last item in itemsToAdd
-        InventoryItem newItem = itemsToAdd.back();
-        itemsToAdd.pop_back();
-
-        for (std::vector<InventoryItem>::iterator it = _inventoryItems.begin(); it != _inventoryItems.end(); ++it){
-            InventoryItem itemInInventory = *it;
-
-            //TODO: look into differentiating derived class and finish this
+//    //add items until vector is empty
+//    while( !itemsToAdd.empty()){
+//
+//        //retrieve last item in itemsToAdd
+//        InventoryItem newItem = itemsToAdd.back();
+//        itemsToAdd.pop_back();
+//
+//        for (std::vector<InventoryItem>::iterator it = _inventoryItems.begin(); it != _inventoryItems.end(); ++it){
+//            InventoryItem itemInInventory = *it;
+//
+//
 //            if(newItem.object.getId() == itemInInventory.object.getId()){
 //                //add item that already exists in inventory
 //                itemInInventory.quantity += newItem.quantity;
@@ -27,24 +28,23 @@ void Inventory::addItems(std::vector<InventoryItem> itemsToAdd){
 //                //add new item
 //                _inventoryItems.push_back(newItem);
 //            }
-        }
-    }
+//        }
+//    }
 }
 
-
+//TODO: look into differentiating derived class and finish this
 bool Inventory::removeItems(std::vector<InventoryItem> itemsToRemove){
-    //remove items until vector is empty
-    while( !itemsToRemove.empty()){
-
-        //retrieve last item in itemsToRemove
-        InventoryItem newItem = itemsToRemove.back();
-        itemsToRemove.pop_back();
-
-        //assumes that the item requested to be removed must exist in inventory
-        for (std::vector<InventoryItem>::iterator it = _inventoryItems.begin(); it != _inventoryItems.end(); ++it){
-            InventoryItem item = *it;
-
-            //TODO: look into differentiating derived class and finish this
+//    //remove items until vector is empty
+//    while( !itemsToRemove.empty()){
+//
+//        //retrieve last item in itemsToRemove
+//        InventoryItem newItem = itemsToRemove.back();
+//        itemsToRemove.pop_back();
+//
+//        //assumes that the item requested to be removed must exist in inventory
+//        for (std::vector<InventoryItem>::iterator it = _inventoryItems.begin(); it != _inventoryItems.end(); ++it){
+//            InventoryItem item = *it;
+//
 //            if(newItem.object.getId() == item.object.getId()){
 //                if(item.quantity >= newItem.quantity){
 //                    //remove item
@@ -55,7 +55,7 @@ bool Inventory::removeItems(std::vector<InventoryItem> itemsToRemove){
 //                }
 //
 //            }
-        }
-    }
-    return true;
+//        }
+//    }
+//    return true;
 }
