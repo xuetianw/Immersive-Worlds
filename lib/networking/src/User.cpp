c814e157e1fd244cba380fdcf51f12f088268e3b
Vincent Lam <vtlam@sfu.ca>
2019-02-06 20:47:55 -0800

    clang tidy

--- lib/networking/src/User.cpp
+++ lib/networking/src/User.cpp
@@ -16,160 +16,137 @@ const string EMPTY_INPUT_PROMPT = "Invalid String - ";
 
 ////////////////////HELPERS//////////////////////////////////
 
-//TODO: Add validity checks asides from empty string
+// TODO: Add validity checks asides from empty string
 
-bool invalidString(const string& str){
-    return str.find_first_not_of(' ') == std::string::npos;
+bool invalidString(const string &str) {
+  return str.find_first_not_of(' ') == std::string::npos;
 }
 
-Message invalidStringMessage(Message oringalMessage, const string& prompt){
-    return Message{oringalMessage.connection, EMPTY_INPUT_PROMPT+prompt};
+Message invalidStringMessage(Message oringalMessage, const string &prompt) {
+  return Message{oringalMessage.connection, EMPTY_INPUT_PROMPT + prompt};
 }
 
 ////////////////////////////////////////////////////////////
 
 class SubmitLoginState : public UserState {
-    bool isSubmittingLoginInfo() override {return true;}
+  bool isSubmittingLoginInfo() override { return true; }
 };
 
 class LoginPasswordState : public UserState {
-    Message handleInput(User& user, Message& message) override {
-        if (invalidString(message.text)){
-            return invalidStringMessage(message, LOGIN_PASSWORD_PROMPT);
-        }
-        user.setPassword(message.text);
-        user.setState(new SubmitLoginState{});
-        return Message{message.connection};
+  Message handleInput(User &user, Message &message) override {
+    if (invalidString(message.text)) {
+      return invalidStringMessage(message, LOGIN_PASSWORD_PROMPT);
     }
+    user.setPassword(message.text);
+    user.setState(new SubmitLoginState{});
+    return Message{message.connection};
+  }
 
-    bool isLoggingIn() override { return true; }
+  bool isLoggingIn() override { return true; }
 };
 
 class LoginUsernameState : public UserState {
-    Message handleInput(User& user, Message& message) override {
-        if (invalidString(message.text)){
-            return invalidStringMessage(message, LOGIN_USERNAME_PROMPT);
-        }
-        user.setUsername(message.text);
-        user.setState(new LoginPasswordState{});
-        return Message{message.connection, LOGIN_PASSWORD_PROMPT};
+  Message handleInput(User &user, Message &message) override {
+    if (invalidString(message.text)) {
+      return invalidStringMessage(message, LOGIN_USERNAME_PROMPT);
     }
+    user.setUsername(message.text);
+    user.setState(new LoginPasswordState{});
+    return Message{message.connection, LOGIN_PASSWORD_PROMPT};
+  }
 
-    bool isLoggingIn() override { return true; }
+  bool isLoggingIn() override { return true; }
 };
 
 class LoggingInState : public UserState {
-    Message handleInput(User& user, Message& message) override {
-        stringstream response;
-        if (message.text.empty()) {
-            user.setState(new LoginUsernameState{});
-            return Message{message.connection, LOGIN_USERNAME_PROMPT};
-        } else {
-            user.setUsername(message.text);
-            user.setState(new LoginPasswordState{});
-            return Message{message.connection, LOGIN_PASSWORD_PROMPT};
-        }
+  Message handleInput(User &user, Message &message) override {
+    stringstream response;
+    if (message.text.empty()) {
+      user.setState(new LoginUsernameState{});
+      return Message{message.connection, LOGIN_USERNAME_PROMPT};
+    } else {
+      user.setUsername(message.text);
+      user.setState(new LoginPasswordState{});
+      return Message{message.connection, LOGIN_PASSWORD_PROMPT};
     }
+  }
 
-    bool isLoggingIn() override { return true; }
+  bool isLoggingIn() override { return true; }
 };
 
-
-class SubmitRegistrationState : public UserState{
-    bool isSubmittingRegistration() override { return true;}
+class SubmitRegistrationState : public UserState {
+  bool isSubmittingRegistration() override { return true; }
 };
 
-class ResgisterPasswordState: public UserState{
-    Message handleInput(User& user, Message& message) override {
-        if (invalidString(message.text)){
-            return invalidStringMessage(message, REGISTER_PASSWORD_PROMPT);
-        }
-        user.setPassword(message.text);
-        user.setState(new SubmitRegistrationState{});
-        return Message{message.connection};
+class ResgisterPasswordState : public UserState {
+  Message handleInput(User &user, Message &message) override {
+    if (invalidString(message.text)) {
+      return invalidStringMessage(message, REGISTER_PASSWORD_PROMPT);
     }
+    user.setPassword(message.text);
+    user.setState(new SubmitRegistrationState{});
+    return Message{message.connection};
+  }
 
-    bool isRegistering() override { return true; };
+  bool isRegistering() override { return true; };
 };
 
-class ResgisterLoginState: public UserState{
-    Message handleInput(User& user, Message& message) override {
-        if (invalidString(message.text)){
-            return invalidStringMessage(message, REGISTER_USERNAME_PROMPT);
-        }
-        user.setUsername(message.text);
-        user.setState(new ResgisterPasswordState);
-        return Message{message.connection, REGISTER_PASSWORD_PROMPT};
+class ResgisterLoginState : public UserState {
+  Message handleInput(User &user, Message &message) override {
+    if (invalidString(message.text)) {
+      return invalidStringMessage(message, REGISTER_USERNAME_PROMPT);
     }
+    user.setUsername(message.text);
+    user.setState(new ResgisterPasswordState);
+    return Message{message.connection, REGISTER_PASSWORD_PROMPT};
+  }
 
-    bool isRegistering() override { return true; };
+  bool isRegistering() override { return true; };
 };
 
-class RegisteringState: public UserState{
-    Message handleInput(User& user, Message& message) override {
-        user.setState(new ResgisterLoginState{});
-        return Message{message.connection, REGISTER_USERNAME_PROMPT};
-     }
+class RegisteringState : public UserState {
+  Message handleInput(User &user, Message &message) override {
+    user.setState(new ResgisterLoginState{});
+    return Message{message.connection, REGISTER_USERNAME_PROMPT};
+  }
 
-    bool isRegistering() override { return true; };
+  bool isRegistering() override { return true; };
 };
 
 class LoggedOutState : public UserState {
-    Message handleInput(User& user, Message& message) override {
+  Message handleInput(User &user, Message &message) override {
     return Message{message.connection, NOT_SIGNED_IN_PROMPT};
   }
 };
 
-void User::promptLogin() {
-  _state = new LoggingInState{};
-}
+void User::promptLogin() { _state = new LoggingInState{}; }
 
-void User::promptRegistration() {
-  _state = new RegisteringState{};
-}
+void User::promptRegistration() { _state = new RegisteringState{}; }
 
-Message User::handleInput(Message& message) {
+Message User::handleInput(Message &message) {
   return _state->handleInput(*this, message);
 }
 
-bool User::isLoggedIn() {
-  return _state->isLoggedInState();
-}
+bool User::isLoggedIn() { return _state->isLoggedInState(); }
 
-bool User::isSubmittingLoginInfo(){
-  return _state->isSubmittingLoginInfo();
-}
+bool User::isSubmittingLoginInfo() { return _state->isSubmittingLoginInfo(); }
 
-bool User::isSubmittingRegistration(){
+bool User::isSubmittingRegistration() {
   return _state->isSubmittingRegistration();
 }
 
-bool User::isLoggingIn() {
-    return _state->isLoggingIn();
-}
+bool User::isLoggingIn() { return _state->isLoggingIn(); }
 
-bool User::isRegistering() {
-    return _state->isRegistering();
-}
+bool User::isRegistering() { return _state->isRegistering(); }
 
-void User::setUsername(const string& username) {
-  User::username = username;
-}
+void User::setUsername(const string &username) { User::username = username; }
 
-void User::setPassword(const string& password) {
-  User::password = password;
-}
+void User::setPassword(const string &password) { User::password = password; }
 
-void User::setState(UserState *state) {
-  User::_state = state;
-}
+void User::setState(UserState *state) { User::_state = state; }
 
 User::User() : username(""), password(""), _state(new LoggedOutState{}) {}
 
-const string& User::getUsername() const {
-  return username;
-}
+const string &User::getUsername() const { return username; }
 
-const string& User::getPassword() const {
-  return password;
-}
+const string &User::getPassword() const { return password; }
