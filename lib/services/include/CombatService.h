3ca0b7a2dc84bbb80ac41bf75f428ed4a077757b
Jacky Lui <jacky_lui_3@sfu.ca>
2019-03-26 21:03:51 -0700

    Revert "Merge branch '74-integrate-combat' into 'development'"
    This reverts merge request !109

--- lib/services/include/CombatService.h
+++ lib/services/include/CombatService.h
@@ -5,37 +5,86 @@
 #ifndef WEBSOCKETNETWORKING_COMBATSERVICE_H
 #define WEBSOCKETNETWORKING_COMBATSERVICE_H
 
-#include <map>
+#include <vector>
 
 #include "Room.h"
 #include "Avatar.h"
-#include "Combat.h"
-
-
-using namespace models;
 
 using namespace std;
+using namespace models;
+
+struct combatRound{
+    int roundNumber;
+    int damageDealtByPlayer;
+    int damageDealtByNPC;
+};
 
 
 /*
  * class for combat logic
  */
-class CombatService{
+class CombatLogic{
 
 private:
 
-    int fightsInitialized;
+    static int fightsInitialized;
+
+    vector<combatRound> rounds;
+    Avatar& player;
+    Avatar& NPC;
+    Room battleRoom;
+
     //obtain value from global config file
-    const static int heartBeatCount = 100;
-
-    //data structure to map user Avatar to his/her own combat
-
-    map<ID*, Combat&> combatMap;
+    constexpr static int heartBeatCount = 100;
+    int roundCount;
 
 public:
 
-    void createCombat(Avatar &player, Avatar& NPC);
-    void destroyCombat(Avatar &player);
+    CombatLogic(Avatar &player, Avatar &NPC);
+
+    /*
+     * attacks an NPC
+     */
+    void attackNpc();
+
+    /*
+     * User takes damage from NPC
+     */
+    void receiveDamage();
+
+    /*
+     * attacks NPC and automatically receives damage from it
+     */
+    void fightRound();
+
+    /*
+     * function which outputs information of the combat state such as
+     * -health of both players
+     * -number of ingame combat rounds
+     * -number of elapsed heartbeats
+     */
+    void displayCombatInfo();
+
+    /*
+     * function to determine if the combat state is still valid
+     * i.e checks if either player is dead or if they decided to flee
+     */
+    bool isCombatActive();
+
+    void updateRoundCount();
+
+    vector<combatRound> &getRounds();
+
+    void setRounds(const vector<combatRound> &rounds);
+
+    Avatar &getPlayer();
+
+    void setPlayer(const Avatar &player);
+
+    Avatar &getNPC();
+
+    void setNPC(const Avatar &NPC);
+
 };
 
 #endif //WEBSOCKETNETWORKING_COMBATSERVICE_H
